2b17dcff86fe9837078a7050231f7f1af5cdb42c
Wellington N <wellingto_07@hotmail.com>
2020-09-11 01:18:11 -0300

    correção create

--- src/products/create.js
+++ src/products/create.js
@@ -6,7 +6,7 @@ const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET,
 });
 
-exports.handle = async (event, context) => {
+exports.handler = async (event, context) => {
   const data = JSON.parse(event.body);
   console.log("Function `create` invoked", data);
   const item = {
