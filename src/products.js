c230d5f30ff702e69eef95bcd73891f11715dec1
Wellington N <wellingto_07@hotmail.com>
2020-09-12 16:37:20 -0300

    control functions

---
+++ src/products.js
@@ -0,0 +1,68 @@
+exports.handler = async (event, context) => {
+  const path = event.path.replace(/\.netlify\/functions\/[^\/]+/, "");
+  const segments = path.split("/").filter((e) => e);
+
+  switch (event.httpMethod) {
+    case "GET":
+      // e.g. GET /.netlify/functions/products
+      if (segments.length === 0) {
+        return require("./products/read-all").handler(event, context);
+      }
+      // e.g. GET /.netlify/functions/products/123456
+      if (segments.length === 1) {
+        event.id = segments[0];
+        return require("./products/read").handler(event, context);
+      } else {
+        return {
+          statusCode: 500,
+          body:
+            "too many segments in GET request, must be either /.netlify/functions/products or /.netlify/functions/products/123456",
+        };
+      }
+    case "POST":
+      // e.g. POST /.netlify/functions/products with a body of key value pair objects, NOT strings
+      return require("./products/create").handler(event, context);
+    case "PUT":
+      // e.g. PUT /.netlify/functions/products/123456 with a body of key value pair objects, NOT strings
+      if (segments.length === 1) {
+        event.id = segments[0];
+        console.log(event.id);
+        return require("./products/update").handler(event, context);
+      } else {
+        return {
+          statusCode: 500,
+          body:
+            "invalid segments in POST request, must be /.netlify/functions/products/123456",
+        };
+      }
+    case "DELETE":
+      // e.g. DELETE /.netlify/functions/products/123456
+      if (segments.length === 1) {
+        event.id = segments[0];
+        return require("./products/delete").handler(event, context);
+      } else {
+        return {
+          statusCode: 500,
+          body:
+            "invalid segments in DELETE request, must be /.netlify/functions/customers/123456",
+        };
+      }
+    case "OPTIONS":
+      // To enable CORS
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      };
+      return {
+        statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
+        headers,
+        body: "This was a preflight call!",
+      };
+  }
+  return {
+    statusCode: 500,
+    body:
+      "unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE/OPTIONS",
+  };
+};
