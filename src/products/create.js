ac5b628b96fec386d0194ce5d4531d6dd362c22e
Wellington N <wellingto_07@hotmail.com>
2020-09-14 09:37:59 -0300

    teste upload

--- src/products/create.js
+++ src/products/create.js
@@ -9,31 +9,31 @@ const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET,
 });
 
-(exports.handler = async(upload.single("image"))),
-  (event, context) => {
-    const data = JSON.parse(event.body);
+exports.handler = async (event, context) => {
+  const data = JSON.parse(event.body);
+  const dataI = upload.single("image");
 
-    console.log("Function `create` invoked", data);
-    const item = {
-      data: data,
-    };
-
-    return client
-      .query(q.Create(q.Ref("classes/products"), item))
-      .then((response) => {
-        console.log("success", response);
-
-        return {
-          statusCode: 200,
-          body: JSON.stringify(response),
-        };
-      })
-      .catch((error) => {
-        console.log("error", error);
-
-        return {
-          statusCode: 400,
-          body: JSON.stringify(error),
-        };
-      });
+  console.log("Function `create` invoked", data);
+  const item = {
+    data: data,
   };
+
+  return client
+    .query(q.Create(q.Ref("classes/products"), item))
+    .then((response) => {
+      console.log("success", response);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response),
+      };
+    })
+    .catch((error) => {
+      console.log("error", error);
+
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      };
+    });
+};
