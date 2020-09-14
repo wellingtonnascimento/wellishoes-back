70d4f791abe4fdf60395562c4d6fd29331dfa9ad
Wellington N <wellingto_07@hotmail.com>
2020-09-14 10:27:44 -0300

    upload image

--- src/products/create.js
+++ src/products/create.js
@@ -11,12 +11,14 @@ const client = new faunadb.Client({
 
 exports.handler = async (event, context) => {
   const data = JSON.parse(event.body);
-  const dataI = upload.single("image");
+  //const dataI = upload.single("image");
 
   console.log("Function `create` invoked", data);
   const item = {
     data: data,
+    image: image,
   };
+  upload.single("data[image]");
 
   return client
     .query(q.Create(q.Ref("classes/products"), item))
