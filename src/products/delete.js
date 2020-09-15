06839a72cc4acc091bc32d59e2ba333bff07efd6
Wellington N <wellingto_07@hotmail.com>
2020-09-15 01:16:30 -0300

    delete

--- src/products/delete.js
+++ src/products/delete.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
   const id = event.id;
   console.log(`Function 'delete' invoked. delete id:: ${id}`);
   return client
-    .query(q.Delete(q.Ref(q.Collection("products"), `classes/products/${id}`)))
+    .query(q.Delete(q.Ref(q.Collection("products"), id)))
     .then((response) => {
       console.log("success", response);
       return {
