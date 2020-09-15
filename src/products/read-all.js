4095d9bf947e9f4e7aace05ed6ddf8814e5a9b11
Wellington N <wellingto_07@hotmail.com>
2020-09-15 09:10:47 -0300

    teste read

--- src/products/read-all.js
+++ src/products/read-all.js
@@ -26,11 +26,7 @@ exports.handler = async (event, context) => {
         });
         return {
           statusCode: 200,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-          },
+
           body: JSON.stringify(wellformedData),
         };
       });
